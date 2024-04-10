492d3a5413cab067467b0d93bba17dc9e88c7691
clisdermar <clisdermar@gmail.com>
2024-04-10 03:33:57 +0000

    2da version home y nabvar

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,6 +3,7 @@ import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import Video from "../../img/maldivesVideo.mp4";
+import {AiOutlineSearch} from 'react-icons/ai'
 
 
 export const Home = () => {
@@ -14,6 +15,19 @@ export const Home = () => {
                 <source src={Video} type='video/mp4' />
             </video>
 			<div className="overlay"></div>
+			<div className="content">
+			     <h1>First Class Travel</h1>
+                <h2>Top 1% Locations Worldwide</h2>
+				<form className="form">
+                    <div>
+                        <input type="text" placeholder='Search Destinations' />
+                    </div>
+                    <div>
+                        <button><AiOutlineSearch className='icon'/></button>
+                    </div>
+                </form>
+
+			</div>
 		</div>
 	);
 }
