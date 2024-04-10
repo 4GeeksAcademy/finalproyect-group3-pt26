20cbb314cf215418978f4ec0441a85cf0114d214
clisdermar <clisdermar@gmail.com>
2024-04-10 21:22:21 +0000

    "video nuevo"

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,6 +2,39 @@ import React, { useContext, useState } from "react"; // Importa useContext y use
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import Video from "../../img/Viaje.mp4";
+import {AiOutlineSearch} from 'react-icons/ai'
+
+
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+
+	return (
+		<div className="home">
+			<video autoPlay loop muted id='video'>
+                <source src={Video} type='video/mp4' />
+            </video>
+			<div className="overlay"></div>
+			<div className="content">
+			     <h1>First Class Travel</h1>
+                <h2>The world is yours, discover it with us</h2>
+				<form className="form">
+                    <div>
+                        <input type="text" placeholder='Search Destinations' />
+                    </div>
+                    <div>
+                        <button><AiOutlineSearch className='icon'/></button>
+                    </div>
+                </form>
+
+			</div>
+		</div>
+	);
+}
+import React, { useContext, useState } from "react"; // Importa useContext y useState en la misma línea
+import { Context } from "../store/appContext";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+import "../../styles/home.css";
 import Video from "../../img/maldivesVideo.mp4";
 import { AiOutlineSearch } from 'react-icons/ai'
 
@@ -16,8 +49,8 @@ export const Home = () => {
 			</video>
 			<div className="overlay"></div>
 			<div className="content">
-			     <h1>First Class Travel</h1>
-                <h2>Top 1% Locations Worldwide</h2>
+				<h1>First Class Travel</h1>
+				<h2>Top 1% Locations Worldwide</h2>
 				<form className="form">
 					<div>
 						<input type="text" placeholder='Search Destinations' />
