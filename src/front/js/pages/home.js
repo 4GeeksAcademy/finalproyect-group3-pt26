18587d239c6d977af0fe2e08c9cc75a6f068b3f3
Anibal-JPG <109487916+Anibal-JPG@users.noreply.github.com>
2024-04-10 20:46:33 +0000

    haciendo el avion

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,7 +3,7 @@ import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import Video from "../../img/maldivesVideo.mp4";
-import {AiOutlineSearch} from 'react-icons/ai'
+import { AiOutlineSearch } from 'react-icons/ai'
 
 
 export const Home = () => {
@@ -12,22 +12,29 @@ export const Home = () => {
 	return (
 		<div className="home">
 			<video autoPlay loop muted id='video'>
-                <source src={Video} type='video/mp4' />
-            </video>
+				<source src={Video} type='video/mp4' />
+			</video>
 			<div className="overlay"></div>
 			<div className="content">
-			     <h1>First Class Travel</h1>
-                <h2>Top 1% Locations Worldwide</h2>
+				<h1>First Class Travel</h1>
+				<h2>Top 1% Locations Worldwide</h2>
 				<form className="form">
-                    <div>
-                        <input type="text" placeholder='Search Destinations' />
-                    </div>
-                    <div>
-                        <button><AiOutlineSearch className='icon'/></button>
-                    </div>
-                </form>
+					<div>
+						<input type="text" placeholder='Search Destinations' />
+					</div>
+					<div>
+						<button><AiOutlineSearch className='icon' /></button>
+					</div>
+				</form>
 
 			</div>
+			// seccion abajo de el video
+			<div className="window-container container d-flex">
+				<div className="window"></div>
+				<div className="window"></div>
+				<div className="window"></div>
+			</div>
+			<div style={{ height: "100px" }} ></div>
 		</div>
 	);
 }
