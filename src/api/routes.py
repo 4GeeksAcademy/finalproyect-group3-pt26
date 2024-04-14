d9a81c44d72e9eecb2f0b81ca10e7684ac60fd45
clisdermar <clisdermar@gmail.com>
2024-04-14 02:36:30 +0000

    "merge"

--- src/api/routes.py
+++ src/api/routes.py
@@ -11,35 +11,11 @@ api = Blueprint('api', __name__)
 # Allow CORS requests to this API
 CORS(api)
 
-
-# @api.route('/hello', methods=['POST', 'GET'])
-# def handle_hello():
-
-#     response_body = {
-#         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-#     }
-
-#     return jsonify(response_body), 200
-
-@api.route('/users', methods= ['GET'])
-def get_user():
-
-    all_users = User.query.all()
-    # print(len(all_users))
-
-    response_body = [user.serialize() for user in all_users]
-    return jsonify(response_body), 200
-
-@api.route('/tours', methods= ['GET'])
-def get_tours():
 @api.route('/users', methods=['GET'])
-def get_users():
-    users = User.query.all()
-    
-    all_tours = Tour.query.all()
-    # print(len(all_users))
+def handle_hello():
 
-    response_body = [user.serialize() for user in all_users]
+    users = User.query.all()
+
     response_body = [user.serialize() for user in users]
-    print(response_body)
+
     return jsonify(response_body), 200
