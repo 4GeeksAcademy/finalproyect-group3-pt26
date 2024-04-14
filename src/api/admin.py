6ed962eab1e3d64301479533009b5bb0c4b3713e
Seba Fretes <84410616+SebaFretes@users.noreply.github.com>
2024-04-14 20:53:39 +0000

    accommodations endpoint created and working on it

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, Tour, Hotel, Paquete
+from .models import db, User, Tour, Hotel, Paquete, Reserva
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -15,6 +15,7 @@ def setup_admin(app):
     admin.add_view(ModelView(Tour, db.session))
     admin.add_view(ModelView(Hotel, db.session))
     admin.add_view(ModelView(Paquete, db.session))
+    admin.add_view(ModelView(Reserva, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
