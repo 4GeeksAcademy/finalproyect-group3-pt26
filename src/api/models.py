2e0209d869c44601c2076cfc764973843644f1a7
Seba Fretes <84410616+SebaFretes@users.noreply.github.com>
2024-04-14 00:18:16 +0000

    changes on models.py tables

--- src/api/models.py
+++ src/api/models.py
@@ -7,6 +7,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -17,6 +18,7 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "username" : username.id,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
