4c7f57abc363ddfd141101d91e9c5ffb85e82124
Seba Fretes <84410616+SebaFretes@users.noreply.github.com>
2024-04-16 15:56:07 +0000

    changes on info view

--- src/front/js/pages/info.js
+++ src/front/js/pages/info.js
@@ -1,96 +1,25 @@
 import React from 'react';
 import "../../styles/home.css";
 
-
 export const Info = () => {
 
 	return (
 		<>
-			<div class="container">
-				<div class="row">
-					<div class="col">
+			<div className="container">
+				<div className="row">
+					<div className="col">
 						<div>
 							<h1>Our company</h1>
 							<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi, deleniti velit labore, veniam corrupti ex aut illo autem aliquam maiores ducimus quibusdam cumque, quis possimus voluptatibus enim voluptatem sunt? Adipisci at nesciunt quasi. Amet quas molestias quam! Deleniti nihil cumque sit dolor. Provident neque laboriosam dignissimos ratione sit vitae tempora, ipsam veritatis fugiat deserunt, maxime repudiandae, nemo inventore reprehenderit error. Ullam, nisi mollitia harum quam quia obcaecati numquam soluta quos maxime sed repellat ratione commodi architecto magni assumenda, officiis repudiandae!</p>
 						</div>
 					</div>
-					<div class="col">
+					<div className="col">
 						<div>
 							<img style={{ maxWidth: '100%', height: 'auto' }} src="https://images.pexels.com/photos/3184339/pexels-photo-3184339.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
 						</div>
 					</div>
 				</div>
 			</div>
-
 		</>
 	);
 }
-
-
-
-
-{/* <div id="carouselExampleControls" className="carousel slide tamañoCarrusel" data-bs-ride="carousel">
-				<div className="carousel-inner">
-					<div className="carousel-item active">
-						<img src="..." className="d-block w-100" alt="..." />
-					</div>
-					<div className="carousel-item">
-						<img src="..." className="d-block w-100" alt="..." />
-					</div>
-					<div className="carousel-item">
-						<img src="..." className="d-block w-100" alt="..." />
-					</div>
-				</div>
-				<button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-					<span className="carousel-control-prev-icon" aria-hidden="true"></span>
-					<span className="visually-hidden">Previous</span>
-				</button>
-				<button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-					<span className="carousel-control-next-icon" aria-hidden="true"></span>
-					<span className="visually-hidden">Next</span>
-				</button>
-			</div>
-
-			<div id="carouselExampleControls" className="carousel slide tamañoCarrusel" data-bs-ride="carousel">
-				<div className="carousel-inner">
-					<div className="carousel-item active">
-						<img src="..." className="d-block w-100" alt="..." />
-					</div>
-					<div className="carousel-item">
-						<img src="..." className="d-block w-100" alt="..." />
-					</div>
-					<div className="carousel-item">
-						<img src="..." className="d-block w-100" alt="..." />
-					</div>
-				</div>
-				<button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-					<span className="carousel-control-prev-icon" aria-hidden="true"></span>
-					<span className="visually-hidden">Previous</span>
-				</button>
-				<button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-					<span className="carousel-control-next-icon" aria-hidden="true"></span>
-					<span className="visually-hidden">Next</span>
-				</button>
-			</div>
-
-			<div id="carouselExampleControls" className="carousel slide tamañoCarrusel" data-bs-ride="carousel">
-				<div className="carousel-inner">
-					<div className="carousel-item active">
-						<img src="..." className="d-block w-100" alt="..." />
-					</div>
-					<div className="carousel-item">
-						<img src="..." className="d-block w-100" alt="..." />
-					</div>
-					<div className="carousel-item">
-						<img src="..." className="d-block w-100" alt="..." />
-					</div>
-				</div>
-				<button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-					<span className="carousel-control-prev-icon" aria-hidden="true"></span>
-					<span className="visually-hidden">Previous</span>
-				</button>
-				<button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-					<span className="carousel-control-next-icon" aria-hidden="true"></span>
-					<span className="visually-hidden">Next</span>
-				</button>
-			</div> */}
