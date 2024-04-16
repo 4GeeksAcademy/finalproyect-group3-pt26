f54e56c9db115c1da0652894d7c47b5fd7be9486
clisdermar <clisdermar@gmail.com>
2024-04-16 17:42:28 +0000

    "Modal de Reservas"

--- src/front/js/pages/singleHotel.js
+++ src/front/js/pages/singleHotel.js
@@ -1,17 +1,8 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 export const SingleHotel = () => {
+   
 
-    const [modal, setModal] = useState(true);
-
-    const openModal = () => {
-        setModal(!modal);
-        console.log(modal);
-    }
-
-    const closeModal = () => {
-        setModal(false)
-    }
 
     return (
         <>
@@ -23,7 +14,38 @@ export const SingleHotel = () => {
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur placeat rem enim suscipit, magnam ducimus vero error? Doloremque voluptatibus harum a accusamus consequuntur officiis minima mollitia inventore minus ex? Similique, veniam autem? Commodi magnam impedit nulla suscipit eius iusto tempore.</p>
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime molestiae dolores perferendis voluptas nobis. Alias libero dicta nisi.</p>
                         <h4 style={{ fontWeight: 'bold', color: '#2e887c' }}>Double room price per night 49$</h4>
-                        <button onClick={openModal} className="btn btn-secondary">Want to reserve? Check your dates and continue</button>
+
+                        <button type="button" className="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                            Want to reserve? Check your dates and continue
+                        </button>
+
+                        {/* <!-- Modal --> */}
+                        <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                            <div className="modal-dialog">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h5 className="modal-title" id="staticBackdropLabel">Reserva</h5>
+                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div className="modal-body">
+                                        <div className="date">
+                                            <div className="input-wrap">
+                                                <label>Check-In</label>
+                                                <input type="date" />
+                                            </div>
+                                            <div className="input-wrap">
+                                                <label>Check-Out</label>
+                                                <input type="date" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button  type="button" class="btn btn-primary">Book</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className="col d-flex justify-content-center align-items-center">
                         <img src="https://images.pexels.com/photos/2771935/pexels-photo-2771935.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -31,28 +53,7 @@ export const SingleHotel = () => {
                             style={{ maxWidth: '80%', height: 'auto' }} />
                     </div>
                 </div>
-
-                {
-                    modal &&
-                    (<div className="modal" tabindex="-1">
-                        <div className="modal-dialog">
-                            <div className="modal-content">
-                                <div className="modal-header">
-                                    <h5 className="modal-title">Modal title</h5>
-                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div className="modal-body">
-                                    <p>Dates</p>
-                                </div>
-                                <div className="modal-footer">
-                                    <button type="button" onClick={closeModal} className="btn btn-secondary" data-bs-dismiss="modal">Book</button>
-                                    <button type="button" onClick={closeModal} className="btn btn-primary">Close</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>)
-                }
             </div>
         </>
-    )
-}
+    );
+};
