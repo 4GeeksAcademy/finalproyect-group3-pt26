e8fcadd8ea2714b441c60e2b4a4cb5c7fdf463b3
clisdermar <clisdermar@gmail.com>
2024-04-16 00:49:15 +0000

    "nabvar,cambio stado"

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -39,14 +39,14 @@ export const Navbar = () => {
             <div className={nav ? 'logo dark' : 'logo'}>
                 <Link to='/'><img src="https://raw.githubusercontent.com/4GeeksAcademy/finalproyect-group3-pt26/main/src/front/img/travello%20white%20bg%20brand.PNG" style={{ width: "150px", height: "55px", opacity: "0.75 " }} /></Link>
             </div>
-            {/* <ul className="nav-menu">
+            <ul className="nav-menu">
                 <li><Link to='/' smooth={true} duration={500}>Home</Link></li>
                 <li><Link to='/info' smooth={true} duration={500} style={{ width: "130px" }}>About Us</Link></li>
 
                 <li><Link to='/travel' smooth={true} duration={500}>Travel</Link></li>
                 <li><Link to='/views' smooth={true} duration={500}>Contact</Link></li>
 
-            </ul> */}
+            </ul>
             <div className="nav-icons">
                 {store.isLogedds ? (
                  <div>
@@ -78,7 +78,7 @@ export const Navbar = () => {
 
             </div>
             <div className={nav ? 'mobile-menu active' : 'mobile-menu'}>
-                {/* <ul className="mobile-nav">
+                <ul className="mobile-nav">
 
                     <li><Link to='/home' smooth={true} duration={500}>Home</Link></li>
                     <li><Link to='/destinations' smooth={true} duration={500}>Destinations</Link></li>
@@ -86,7 +86,7 @@ export const Navbar = () => {
                     <li><Link to='/login' smooth={true} duration={500}>Book</Link></li>
 
 
-                </ul> */}
+                </ul>
                 <div className="mobile-menu-bottom">
                     <div className="menu-icons">
                         <button>Search</button>
