d10b8a32dd8bef604c3776493ecfc08235cec361
clisdermar <clisdermar@gmail.com>
2024-04-19 00:10:09 +0000

    "Accommodations listo"

--- src/front/js/pages/accommodations.js
+++ src/front/js/pages/accommodations.js
@@ -1,152 +1,35 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
 export const Accommodations = () => {
+    const { store, actions } = useContext(Context);
 
-	return (
-		<>
-			<div className="container">
-				<h1>Hotels</h1>
+    useEffect(() => {
+        actions.hoteles();
+    }, [actions]);
 
-				<div className="card-group" style={{ marginBottom: '2rem' }}>
-					<div className="card p-3">
-						<img src="https://images.pexels.com/photos/2771935/pexels-photo-2771935.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="..." />
-						<div className="card-body">
-							<h5 className="card-title">Hotel Title</h5>
-							<div className="d-flex">
-								<ion-icon name="location-outline"></ion-icon>
-								<p>Cancún, México</p>
-							</div>
-							<p className="card-text">This hotel Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia error aspernatur minus.</p>
-							<Link to="/accommodation/">
-								<button className="btn btn-primary">VIEW HOTEL</button>
-							</Link>
-						</div>
-					</div>
-					<div className="card p-3">
-						<img src="https://images.pexels.com/photos/9119622/pexels-photo-9119622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="..." />
-						<div className="card-body">
-							<h5 className="card-title">Hotel Title</h5>
-							<div className="d-flex">
-								<ion-icon name="location-outline"></ion-icon>
-								<p>Tulum, México</p>
-							</div>
-							<p className="card-text">This hotel Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, quo architecto.</p>
-							<Link to="/accommodation/">
-								<button className="btn btn-primary">VIEW HOTEL</button>
-							</Link>
-						</div>
-					</div>
-					<div className="card p-3">
-						<img src="https://images.pexels.com/photos/9259539/pexels-photo-9259539.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="..." />
-						<div className="card-body">
-							<h5 className="card-title">Hotel Title</h5>
-							<div className="d-flex">
-								<ion-icon name="location-outline"></ion-icon>
-								<p>Playa del Carmen, México</p>
-							</div>
-							<p className="card-text">This hotel Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus mollitia dicta consequatur dolorum non.</p>
-							<Link to="/accommodation/">
-								<button className="btn btn-primary">VIEW HOTEL</button>
-							</Link>
-						</div>
-					</div>
-				</div>
-
-				<div className="card-group" style={{ marginBottom: '2rem' }}>
-					<div className="card p-3">
-						<img src="https://images.pexels.com/photos/7372480/pexels-photo-7372480.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="..." />
-						<div className="card-body">
-							<h5 className="card-title">Hotel Title</h5>
-							<div className="d-flex">
-								<ion-icon name="location-outline"></ion-icon>
-								<p>Cancún, México</p>
-							</div>
-							<p className="card-text">This hotel Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia error aspernatur minus.</p>
-							<Link to="/accommodation/">
-								<button className="btn btn-primary">VIEW HOTEL</button>
-							</Link>
-						</div>
-					</div>
-					<div className="card p-3">
-						<img src="https://images.pexels.com/photos/12565208/pexels-photo-12565208.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="..." />
-						<div className="card-body">
-							<h5 className="card-title">Hotel Title</h5>
-							<div className="d-flex">
-								<ion-icon name="location-outline"></ion-icon>
-								<p>Cancún, México</p>
-							</div>
-							<p className="card-text">This hotel Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, quo architecto.</p>
-							<Link to="/accommodation/">
-								<button className="btn btn-primary">VIEW HOTEL</button>
-							</Link>
-						</div>
-					</div>
-					<div className="card p-3">
-						<img src="https://images.pexels.com/photos/5259756/pexels-photo-5259756.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="..." />
-						<div className="card-body">
-							<h5 className="card-title">Hotel Title</h5>
-							<div className="d-flex">
-								<ion-icon name="location-outline"></ion-icon>
-								<p>Cancún, México</p>
-							</div>
-							<p className="card-text">This hotel Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus mollitia dicta consequatur dolorum non.</p>
-							<Link to="/accommodation/">
-								<button className="btn btn-primary">VIEW HOTEL</button>
-							</Link>
-						</div>
-					</div>
-
-				</div>
-
-				<div className="card-group" style={{ marginBottom: '2rem' }}>
-					<div className="card p-3">
-						<img src="https://images.pexels.com/photos/12588195/pexels-photo-12588195.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="..." />
-						<div className="card-body">
-							<h5 className="card-title">Hotel Title</h5>
-							<div className="d-flex">
-								<ion-icon name="location-outline"></ion-icon>
-								<p>Cancún, México</p>
-							</div>
-							<p className="card-text">This hotel Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia error aspernatur minus.</p>
-							<Link to="/accommodation/">
-								<button className="btn btn-primary">VIEW HOTEL</button>
-							</Link>
-						</div>
-					</div>
-					<div className="card p-3">
-						<img src="https://images.pexels.com/photos/17000989/pexels-photo-17000989/free-photo-of-calle-hotel-verano-urbano.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="..." />
-						<div className="card-body">
-							<h5 className="card-title">Hotel Title</h5>
-							<div className="d-flex">
-								<ion-icon name="location-outline"></ion-icon>
-								<p>Cancún, México</p>
-							</div>
-							<p className="card-text">This hotel Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, quo architecto.</p>
-							<Link to="/accommodation/">
-								<button className="btn btn-primary">VIEW HOTEL</button>
-							</Link>
-						</div>
-					</div>
-					<div className="card p-3">
-						<img src="https://images.pexels.com/photos/15962382/pexels-photo-15962382/free-photo-of-ciudad-hotel-ventanas-urbano.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="..." />
-						<div className="card-body">
-							<h5 className="card-title">Hotel Title</h5>
-							<div className="d-flex">
-								<ion-icon name="location-outline"></ion-icon>
-								<p>Cancún, México</p>
-							</div>
-							<p className="card-text">This hotel Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus mollitia dicta consequatur dolorum non.</p>
-							<Link to="/accommodation/">
-								<button className="btn btn-primary">VIEW HOTEL</button>
-							</Link>
-						</div>
-					</div>
-
-				</div>
-
-
-			</div>
-		</>
-	);
+    return (
+        <div className="container">
+            <h1>Accommodations</h1>
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+                {store.hoteles.map((hotel, id) => (
+                    <div className="col" key={id}>
+                        <div className="card">
+                            <img src="https://images.pexels.com/photos/338504/pexels-photo-338504.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">{hotel.name}</h5>
+                                <p className="card-text">{hotel.descripcion}</p>
+                                <h3 className="card-title">{hotel.duracion}</h3>
+                                <h4 className="card-title">{hotel.precio}</h4>
+                                <Link to="/package/">
+                                    <button className="btn btn-primary">VIEW DETAILS</button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 };
