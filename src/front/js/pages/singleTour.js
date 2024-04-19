b2cd0252e0605b3c51abc1812c0498f9846dee4f
Seba Fretes <84410616+SebaFretes@users.noreply.github.com>
2024-04-19 04:36:58 +0000

    singleTour fetch done

--- src/front/js/pages/singleTour.js
+++ src/front/js/pages/singleTour.js
@@ -45,39 +45,9 @@ export const SingleTour = () => {
                                         </div>
                                     }
                                 </div>
-
                                 <button type="button" className="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                                     Book
                                 </button>
-
-
-                                {/* <!-- Modal --> */}
-                                <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                                    <div className="modal-dialog modal-dialog-centered">
-                                        <div className="modal-content">
-                                            <div className="modal-header">
-                                                <h5 className="modal-title" id="staticBackdropLabel">Reserva</h5>
-                                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                            </div>
-                                            <div className="modal-body">
-                                                <div className="date">
-                                                    <div className="input-wrap">
-                                                        <label>Check-In</label>
-                                                        <input type="date" />
-                                                    </div>
-                                                    <div className="input-wrap">
-                                                        <label>Check-Out</label>
-                                                        <input type="date" />
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="modal-footer">
-                                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                                <button type="button" className="btn btn-primary">Book</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
                             </div>
                         </div>
                     </div>
