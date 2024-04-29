305a2f8328ad11139cd8d7b123d5027a27e98d11
Anibal-JPG <109487916+Anibal-JPG@users.noreply.github.com>
2024-04-29 22:25:20 +0000

    se mejoro la responsividad de contactos

--- src/front/js/pages/contact.js
+++ src/front/js/pages/contact.js
@@ -4,92 +4,96 @@ import { FaFacebook, FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa'
 import { Link } from "react-router-dom";
 export const Contact = () => {
     return (<>
-        <div className="container text-center" style={{ "display": "contents" }}>
+        <div className="container text-center">
             <div className="row CONrow">
-                <div className="col-7 CONbox">
+                <div className="col-md-7 CONbox">
                     <div className="row">
-                        <div className="col-6" style={{ "padding": "5px" }}>
+                        <div className="col-md-6" style={{ padding: "5px" }}>
                             <div className="row">
                                 <h2 className="col CONname">Clisdermar</h2>
-                                <div className="row">
+                                <div className="row  CONresp">
                                     <div className="col-4 ConSpace"> <Link to="/"><FaFacebook className="CONicon" /></Link></div>
                                     <div className="col-4 ConSpace"> <Link to="/"><FaInstagram className="CONicon" /></Link></div>
                                     <div className="col-4 ConSpace"> <Link to="/"><FaLinkedin className="CONicon" /></Link></div>
                                     <div className="col-4 ConSpace"> <Link to="/"><FaGithub className="CONicon" /> </Link> </div>
-                                    <div className="col d-flex justify-content-center"><h4>Dev Fullstack</h4></div>
+                                    <div className="col d-flex justify-content-center"><h4 className="h4Resp" >Dev Fullstack</h4></div>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-6" style={{ "padding": "5px" }}><img className="CONimg" src="https://ca.slack-edge.com/T0BFXMWMV-U066UAJES95-af7e07c113c8-512" alt="Clisdermar" /></div>
+                        <div className="col-md-6 d-none d-md-block" style={{ padding: "5px" }}>
+                            <img className="CONimg img-fluid" src="https://ca.slack-edge.com/T0BFXMWMV-U066UAJES95-af7e07c113c8-512" alt="Clisdermar" style={{ maxWidth: "100%", maxHeight: "100%" }} />
+                        </div>
                     </div>
                 </div>
-                <div className="col-5 d-flex align-items-center"><div className="CONcontacto">Contacta con Un Asesor</div></div>
+                <div className="col-md-5 d-flex align-items-center"><div className="CONcontacto">Contacta con Un Asesor</div></div>
             </div>
             <div className="row CONrow d-flex align-items-center">
-                <div className="col-5">
-                    {/* FORMULARIO */}
+                <div className="col-md-5">
                     <div className="row justify-content-center">
-                        <div className="col-md-8">
+                        <div className="">
                             <div className="form-container">
                                 <h4>Escribenos!</h4>
                                 <form>
                                     <div className="form-group">
-                                        <label for="nombre"></label>
+                                        <label htmlFor="nombre"></label>
                                         <input type="text" className="form-control" id="nombre" placeholder="Ingrese su nombre" />
                                     </div>
                                     <div className="form-group">
-                                        <label for="email"></label>
+                                        <label htmlFor="email"></label>
                                         <input type="email" className="form-control" id="email" placeholder="Ingrese su correo electrónico" />
                                     </div>
                                     <div className="form-group">
-                                        <label for="mensaje"></label>
+                                        <label htmlFor="mensaje"></label>
                                         <textarea className="form-control" id="mensaje" rows="4" placeholder="Ingrese su mensaje"></textarea>
                                     </div>
-                                    <button style={{ "marginTop": "10px" }} type="submit" className="btn">Enviar</button>
+                                    <button style={{ marginTop: "10px" }} type="submit" className="btn">Enviar</button>
                                 </form>
                             </div>
                         </div>
                     </div>
-                    {/* FORMULARIO */}
                 </div>
-                <div className="col-7 CONbox" style={{ "boxShadow": "12px 12px 1px rgba(70, 206, 235, 1.0)", "height": "220px" }}>
+                <div className="col-md-7 CONbox" style={{ boxShadow: "12px 12px 1px rgba(70, 206, 235, 1.0)", height: "220px" }}>
                     <div className="row">
-                        <div className="col-6" style={{ "padding": "5px" }}>
+                        <div className="col-md-6" style={{ padding: "5px" }}>
                             <div className="row">
                                 <h2 className="col CONname">Sebastian</h2>
-                                <div className="row">
+                                <div className="row  CONresp">
                                     <div className="col-4 ConSpace"> <Link to="/"><FaFacebook className="CONicon" /></Link></div>
-                                    <div className="col-4 ConSpace"> <Link to=""><FaInstagram className="CONicon" /></Link></div>
-                                    <div className="col-4 ConSpace"> <Link to=""><FaLinkedin className="CONicon" /></Link></div>
-                                    <div className="col-4 ConSpace"> <Link to=""><FaGithub className="CONicon" /> </Link> </div>
-                                    <div className="col d-flex justify-content-center"><h4>Dev Fullstack</h4></div>
+                                    <div className="col-4 ConSpace"> <Link to="/"><FaInstagram className="CONicon" /></Link></div>
+                                    <div className="col-4 ConSpace"> <Link to="/"><FaLinkedin className="CONicon" /></Link></div>
+                                    <div className="col-4 ConSpace"> <Link to="/"><FaGithub className="CONicon" /> </Link> </div>
+                                    <div className="col d-flex justify-content-center"><h4 className="h4Resp" >Dev Fullstack</h4></div>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-6" style={{ "padding": "5px" }}><img className="CONimg" src="https://ca.slack-edge.com/T0BFXMWMV-U06701S2X7Z-be6f4d35fd2e-512" alt="Sebastian" /></div>
+                        <div className="col-md-6 d-none d-md-block" style={{ padding: "5px" }}>
+                            <img className="CONimg img-fluid" src="https://ca.slack-edge.com/T0BFXMWMV-U06701S2X7Z-be6f4d35fd2e-512" alt="Sebastian" style={{ maxWidth: "100%", maxHeight: "100%" }} />
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="row CONrow" style={{ "marginBottom": "78px" }}>
-                <div className="col-7 CONbox">
+            <div className="row CONrow" style={{ marginBottom: "78px" }}>
+                <div className="col-md-7 CONbox">
                     <div className="row">
-                        <div className="col-6" style={{ "padding": "5px" }}>
-                            <div className="row">
+                        <div className="col-md-6" style={{ padding: "5px" }}>
+                            <div className="row" style={{ margin: "6px" }}>
                                 <h2 className="col CONname">Anibal</h2>
                             </div>
-                            <div className="row">
+                            <div className="row CONresp">
                                 <div className="col-4 ConSpace"> <Link to="https://www.facebook.com/anibal.velasco.1257/"><FaFacebook className="CONicon" /></Link></div>
                                 <div className="col-4 ConSpace"> <Link to="https://www.instagram.com/anibal_jpeg/"><FaInstagram className="CONicon" /></Link></div>
                                 <div className="col-4 ConSpace"> <Link to="https://www.linkedin.com/in/anibal-crespo-446564241/"><FaLinkedin className="CONicon" /></Link></div>
                                 <div className="col-4 ConSpace"> <Link to="https://github.com/Anibal-JPG"><FaGithub className="CONicon" /> </Link> </div>
-                                <div className="col d-flex justify-content-center"><h4>Dev Fullstack</h4></div>
+                                <div className="col d-flex justify-content-center"><h4 className="h4Resp" >Dev Fullstack</h4></div>
                             </div>
                         </div>
-                        <div className="col-6" style={{ "padding": "5px" }}><img className="CONimg" src="https://ca.slack-edge.com/T0BFXMWMV-U05R1KG2DJA-9d8c1dd43555-512" alt="Anibal" /></div>
+                        <div className="col-md-6 d-none d-md-block" style={{ padding: "5px" }}>
+                            <img className="CONimg img-fluid" src="https://ca.slack-edge.com/T0BFXMWMV-U05R1KG2DJA-9d8c1dd43555-512" alt="Anibal" style={{ maxWidth: "100%", maxHeight: "100%" }} />
+                        </div>
                     </div>
                 </div>
-                <div className="col-5"></div>
+                <div className="col-md-5"></div>
             </div>
-        </div>
+        </div>;
     </>)
 }
