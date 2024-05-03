8b210668deeecab578dbf287157be426911151d6
Seba Fretes <84410616+SebaFretes@users.noreply.github.com>
2024-05-03 19:51:08 +0000

    buttons added on adminTour.js view

--- src/front/js/pages/adminTour.js
+++ src/front/js/pages/adminTour.js
@@ -71,70 +71,79 @@ export const AdminTour = () => {
   };
 
   return (
-    <div className="d-flex justify-content-center ">
-      <div className="card border border-3" style={{ width: '30rem' }} >
-        <img className="rounded-0 img-fluid" src="https://lesroches.edu/wp-content/uploads/2023/03/tour-1.png" alt="..." />
-        <div className="card-body">
-          <input type="texto" className="form-control"
-            placeholder="Name" style={{ width: '300px', height: '30px', borderRadius: '0' }}
-            value={tourData.name}
-            onChange={(e) => setTourData({ ...tourData, name: e.target.value })}
-          />
-        </div>
-        <ul className="list-group list-group-flush">
-          <textarea className="form-control mb-3" id="exampleFormControlTextarea1" rows="3"
-            placeholder="Description"
-            style={{ borderRadius: '0' }}
-            value={tourData.descripcion}
-            onChange={(e) => setTourData({ ...tourData, descripcion: e.target.value })}
-          ></textarea>
-          <li> <input type="texto" className="form-control" placeholder="Duracion"
-            style={{ width: '300px', height: '30px', borderRadius: '0' }}
+    <>
+      <div className="d-flex justify-content-center ">
+        <div className="card border border-3" style={{ width: '30rem' }} >
+          <img className="rounded-0 img-fluid" src="https://lesroches.edu/wp-content/uploads/2023/03/tour-1.png" alt="..." />
+          <div className="card-body">
+            <input type="texto" className="form-control"
+              placeholder="Name" style={{ width: '300px', height: '30px', borderRadius: '0' }}
+              value={tourData.name}
+              onChange={(e) => setTourData({ ...tourData, name: e.target.value })}
+            />
+          </div>
+          <ul className="list-group list-group-flush">
+            <textarea className="form-control mb-3" id="exampleFormControlTextarea1" rows="3"
+              placeholder="Description"
+              style={{ borderRadius: '0' }}
+              value={tourData.descripcion}
+              onChange={(e) => setTourData({ ...tourData, descripcion: e.target.value })}
+            ></textarea>
+            <li> <input type="texto" className="form-control" placeholder="Duracion"
+              style={{ width: '300px', height: '30px', borderRadius: '0' }}
 
-            value={tourData.duracion}
-            onChange={(e) => setTourData({ ...tourData, duracion: e.target.value })} />
-          </li>
-          <li><input type="texto" className="form-control"
-            placeholder="Precio" style={{ width: '100px', height: '30px', borderRadius: '0' }}
-            value={tourData.precio}
-            onChange={(e) => setTourData({ ...tourData, precio: e.target.value })}
-          />
-          </li>
-        </ul>
+              value={tourData.duracion}
+              onChange={(e) => setTourData({ ...tourData, duracion: e.target.value })} />
+            </li>
+            <li><input type="texto" className="form-control"
+              placeholder="Precio" style={{ width: '100px', height: '30px', borderRadius: '0' }}
+              value={tourData.precio}
+              onChange={(e) => setTourData({ ...tourData, precio: e.target.value })}
+            />
+            </li>
+          </ul>
 
-        <div>
-          <input type="file" onChange={e => handleFiles(e.target.files)} />
-          {file &&
-            <img src={URL.createObjectURL(file)} alt="img-preview" style={{ maxWidth: '30rem' }} />
-          }
-          {serverResponse}
-          <button onClick={() => handleSubmit()} className="btn btn-success">Upload</button>
-        </div>
+          <div>
+            <input type="file" onChange={e => handleFiles(e.target.files)} />
+            {file &&
+              <img src={URL.createObjectURL(file)} alt="img-preview" style={{ maxWidth: '30rem' }} />
+            }
+            {serverResponse}
+            <button onClick={() => handleSubmit()} className="btn btn-success">Upload</button>
+          </div>
 
 
-        <div className="card-body">
-          {/* <!-- Button trigger modal --> */}
-          <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={handleCreateTour}>
-            Create Tour
-          </button>
+          <div className="card-body">
+            {/* <!-- Button trigger modal --> */}
+            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={handleCreateTour}>
+              Create Tour
+            </button>
 
-          {/* <!-- Modal --> */}
-          <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h5 className="modal-title" id="exampleModalLabel"> Tour Creado Exitosamente</h5>
-                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div className="modal-footer">
-                  <button type="button" className="btn btn-primary" data-bs-dismiss="modal" >Close</button>
+            {/* <!-- Modal --> */}
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel"> Tour Creado Exitosamente</h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-primary" data-bs-dismiss="modal" >Close</button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-
+      <div className="d-flex justify-content-between" style={{ width: '30rem', margin: 'auto', paddingTop: '1rem', paddingBottom: '1rem' }}>
+        <Link to={'/admin'}>
+          <button type="button" className="btn btn-secondary">Go back</button>
+        </Link>
+        <Link to={'/tours'}>
+          <button type="button" className="btn btn-secondary">Tours view</button>
+        </Link>
+      </div>
+    </>
   );
 };
