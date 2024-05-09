d56c8dfc23d7499105325ad43dd3a6e232b2f5e5
Seba Fretes <84410616+SebaFretes@users.noreply.github.com>
2024-05-09 23:59:12 +0000

    packages & tours views styles modified

--- src/front/js/pages/packages.js
+++ src/front/js/pages/packages.js
@@ -14,14 +14,31 @@ export const Packages = () => {
             <h1 style={{ textAlign: 'center' }}>Packages</h1>
             <div className="row row-cols-1 row-cols-md-2 g-4">
                 {store.paquetes.map((pkg, id) => (
+                    // <div className="col" key={id}>
+                    //     <div className="card">
+                    //         <img src={pkg.imageURL} className="card-img-top" alt="..." />
+                    //         <div className="card-body">
+                    //             <h5 className="card-title">{pkg.name}</h5>
+                    //             <p className="card-text">{pkg.descripcion}</p>
+                    //             <Link to={`/package/${pkg.id}`}>
+                    //                 <button className="btn btn-primary">VIEW DETAILS</button>
+                    //             </Link>
+                    //         </div>
+                    //     </div>
+                    // </div>
+
+
                     <div className="col" key={id}>
                         <div className="card">
                             <img src={pkg.imageURL} className="card-img-top" alt="..." />
                             <div className="card-body">
-                                <h5 className="card-title">{pkg.name}</h5>
-                                <p className="card-text">{pkg.descripcion}</p>
+                                <h5 className="card-title" style={{ color: '#3a6b67', fontSize: '2rem', fontWeight: '700' }}>{pkg.name}</h5>
+                                <p className="card-text" style={{ fontSize: '1.4rem', lineHeight: '1.2' }}>{pkg.destino} days</p>
+                                <p className="card-text" style={{ fontSize: '1rem', lineHeight: '1.2' }}>{pkg.descripcion}</p>
+                                <p className="card-text" style={{ fontSize: '1.5rem', fontWeight: '500', color: '#464f52', lineHeight: '1.2' }}>{pkg.precio} $</p>
+                                <p className="card-text" style={{ fontSize: '1rem', lineHeight: '1.2' }}>Taxes included</p>
                                 <Link to={`/package/${pkg.id}`}>
-                                    <button className="btn btn-primary">VIEW DETAILS</button>
+                                    <button>VIEW DETAILS</button>
                                 </Link>
                             </div>
                         </div>
--- src/front/js/pages/tours.js
+++ src/front/js/pages/tours.js
@@ -18,9 +18,10 @@ export const Tours = () => {
                         <div className="card">
                             <img src={tour.imageURL} className="card-img-top" alt="..." />
                             <div className="card-body">
-                                <h5 className="card-title">{tour.name}</h5>
-                                <p className="card-text">{tour.descripcion}</p>
-                                <p className="card-title">{tour.precio} $ per person</p>
+                                <h5 className="card-title" style={{ color: '#3a6b67', fontSize: '2rem', fontWeight: '700' }}>{tour.name}</h5>
+                                <p className="card-text" style={{ fontSize: '1rem', lineHeight: '1.2' }}>{tour.descripcion}</p>
+                                <p className="card-text" style={{ fontSize: '1.5rem', fontWeight: '500', color: '#464f52', lineHeight: '1.2' }}>{tour.precio} $</p>
+                                <p className="card-text" style={{ fontSize: '1rem', lineHeight: '1.2' }}>Taxes included</p>
                                 <Link to={`/tour/${tour.id}`}>
                                     <button>VIEW DETAILS</button>
                                 </Link>
